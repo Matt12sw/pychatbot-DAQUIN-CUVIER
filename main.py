b2772ba5c960a6c943c9144ed58795413e23705c
azur424 <adrien94360@gmail.com>
2023-11-29 18:24:46 +0100

    extraction des nom corrections

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
 import os
 
-def extraire_et_renommer_president(nom_fichier):
-    # Liste des noms de présidents
-    presidents = ["Chirac", "Giscard d'Estaing", "Mitterrand", "Macron", "Sarkozy"]
 
-    # Extraire le nom du président à partir du nom du fichier
-    nom_president = nom_fichier.split("_")[1]
-
-    # Vérifier si le nom du président extrait est dans la liste des noms de présidents
-    if nom_president in presidents:
-        # Construire le nouveau nom du fichier en remplaçant le numéro optionnel par le nom du président
-        nouveau_nom = f"{nom_fichier.split('_')[0]}_{nom_president}{nom_fichier.split('_')[2]}"
-
-        # Construire le chemin complet du fichier actuel et du nouveau fichier
-        chemin_actuel = os.path.join("speeches", nom_fichier)
-        chemin_nouveau = os.path.join("speeches", nouveau_nom)
-
-        # Renommer le fichier
-        os.rename(chemin_actuel, chemin_nouveau)
-
-        print(f"Le fichier {nom_fichier} a été renommé en {nouveau_nom}")
+def extraire_nom_president(nom_fichier):
+    # Supprimer le préfixe "Nomination_" et l'extension ".txt" du nom du fichier
+    nom_president = nom_fichier.replace("Nomination_", "").replace(".txt", "")
+    return nom_president
 
 
-# Se déplacer dans le répertoire "speeches"
-os.chdir("speeches")
+# Chemin du dossier contenant les fichiers
+dossier = "speeches"
 
-# Parcourir tous les fichiers dans le répertoire
-for nom_fichier in os.listdir("."):
-    extraire_et_renommer_president(nom_fichier)
+# Parcourir tous les fichiers dans le dossier
+for nom_fichier in os.listdir(dossier):
+    chemin_fichier = os.path.join(dossier, nom_fichier)
 
-# Afficher le nouveau nom de chaque fichier à la fin
-print("\nNouveaux noms des fichiers :")
-for nom_fichier in os.listdir("."):
-    print(nom_fichier)
+    # Vérifier si le chemin correspond à un fichier (et non à un sous-dossier)
+    if os.path.isfile(chemin_fichier):
+        # Extraire le nom du président à partir du nom du fichier
+        nom_president = extraire_nom_president(nom_fichier)
+
+        # Afficher le nom du président associé à chaque fichier
+        print(f"Le fichier {nom_fichier} correspond au président {nom_president}")
