653725af378ca575312f2a79c6f97a9370d9d03d
azur424 <adrien94360@gmail.com>
2023-12-03 23:33:44 +0100

    extraction des nom ,listes des presidents , convertion des fichier en fichier tt en minuscule dans un dossier cleaned,nettoyage des fichiers cleaned , fonction IDF

--- main.py
+++ main.py
@@ -1,78 +1,110 @@
-#madeleines
+
 import os
+import math
 def extraire_nom_president(nom_fichier):
-    # Supprimer le préfixe "Nomination_" et l'extension ".txt" du nom du fichier
+
     nom_president = nom_fichier.replace("Nomination_", "").replace(".txt", "").replace("1", "").replace("2", "")
     return nom_president
 
-# Chemin du dossier contenant les fichiers
 dossier = "speeches"
 
-# Parcourir tous les fichiers dans le dossier
 for nom_fichier in os.listdir(dossier):
     chemin_fichier = os.path.join(dossier, nom_fichier)
 
-    # Vérifier si le chemin correspond à un fichier (et non à un sous-dossier)
     if os.path.isfile(chemin_fichier):
-        # Extraire le nom du président à partir du nom du fichier
         nom_president = extraire_nom_president(nom_fichier)
-
-        # Afficher le nom du président associé à chaque fichier
-        print(f"Le fichier {nom_fichier} correspond au président {nom_president}")
+        print("Le fichier",nom_fichier," correspond au président ",nom_president)
 
 def liste_noms_presidents(dossier):
-    # Initialiser une liste pour stocker les noms des présidents
+
     noms_presidents = []
 
-    # Parcourir tous les fichiers dans le dossier
     for nom_fichier in os.listdir(dossier):
         chemin_fichier = os.path.join(dossier, nom_fichier)
 
-        # Vérifier si le chemin correspond à un fichier (et non à un sous-dossier)
         if os.path.isfile(chemin_fichier):
-            # Extraire le nom du président à partir du nom du fichier
             nom_president = extraire_nom_president(nom_fichier)
 
-            # Ajouter le nom du président à la liste s'il n'est pas déjà présent
             if nom_president not in noms_presidents:
                 noms_presidents.append(nom_president)
 
     return noms_presidents
 
-# Appeler la fonction pour obtenir la liste des noms des présidents dans le dossier "speeches"
 noms_presidents = liste_noms_presidents("speeches")
 
-# Afficher la liste des noms des présidents
 print("Noms des présidents dans le dossier 'speeches':", noms_presidents)
 
 def convertir_et_enregistrer_en_minuscules(dossier_entree, dossier_sortie):
 
-
-    # Liste des fichiers dans le dossier d'entrée
     fichiers = os.listdir(dossier_entree)
 
     for fichier in fichiers:
         chemin_fichier_entree = os.path.join(dossier_entree, fichier)
 
-        # Vérifiez si le chemin est un fichier
         if os.path.isfile(chemin_fichier_entree):
             with open(chemin_fichier_entree, 'r') as file:
                 contenu = file.read()
-
-            # Convertir le contenu en minuscules
             contenu_minuscules = contenu.lower()
-
-            # Créer le chemin du fichier de sortie
             chemin_fichier_sortie = os.path.join(dossier_sortie, fichier)
 
-            # Enregistrer le fichier en minuscules dans le dossier de sortie
             with open(chemin_fichier_sortie, 'w') as file:
                 file.write(contenu_minuscules)
+            print("Le fichier",fichier,"a été converti et enregistré en minuscules.")
 
-            print(f'Le fichier {fichier} a été converti et enregistré en minuscules.')
-
-# Utilisation de la fonction
 dossier_entree = 'speeches'
 dossier_sortie = 'cleaned'
 
 convertir_et_enregistrer_en_minuscules(dossier_entree, dossier_sortie)
+
+def nettoyer_le_texte(dossier_entree, dossier_sortie):
+
+    fichiers = os.listdir(dossier_entree)
+
+    for fichier in fichiers:
+        chemin_fichier_entree = os.path.join(dossier_entree, fichier)
+
+        if os.path.isfile(chemin_fichier_entree):
+            with open(chemin_fichier_entree, 'r') as file:
+                contenu = file.read()
+            contenu_nettoyer = contenu.replace("?", "").replace(",", "").replace("-", "").replace(".", "").replace("!", "").replace("'", "")
+            chemin_fichier_sortie = os.path.join(dossier_sortie, fichier)
+
+            with open(chemin_fichier_sortie, 'w') as file:
+                file.write(contenu_nettoyer)
+            print("Le fichier",fichier," a été nettoyer et enregistré .")
+
+dossier_entree = 'cleaned'
+dossier_sortie = 'cleaned'
+
+nettoyer_le_texte(dossier_entree, dossier_sortie)
+
+
+def IDF(directory):
+    os.chdir(directory)
+    nb_fichier=0
+    texte=""
+    nb_mots=0
+    IDF_mots=0
+    for fichier in os.listdir(directory):
+        nb_fichier+=1
+        dico={}
+        with open(r'/Users/adrien/PycharmProjects/pychatbot-DAQUIN-CUVIER/cleaned/{files}'.format(file=files),"r")as file:
+            texte+=file.read()
+            file.close()
+            for mots in text.split():
+                if mots not in dico:
+                    for files in os.listdir(directory):
+                        with open(r'/Users/adrien/PycharmProjects/pychatbot-DAQUIN-CUVIER/cleaned/{files}'.format(file=files),"r")as file:
+                            if mots in file.read():
+                                nb_mots+=1
+
+            IDF_mots=math.log((nb_fichier/nb_mots))
+            nb_mots=0
+            dico.update({mots:IDF_mots})
+            return(dico)
+
+
+
+
+
+
